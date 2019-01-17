09218b77d66268fd68734031ced85c0eafa00a26
Stefan Hudelmaier <stefan.hudelmaier@device-insight.com>
2019-01-17 14:40:15 +0100

    Some minor improvements for the help of the consume cmd

--- cmd/consume/consume.go
+++ cmd/consume/consume.go
@@ -8,18 +8,18 @@ import (
 var flags operations.ConsumerFlags
 
 var CmdConsume = &cobra.Command{
-	Use:   "consume",
+	Use:   "consume TOPIC",
 	Short: "consume messages from a topic",
-	Args:  cobra.MinimumNArgs(1),
+	Args:  cobra.ExactArgs(1),
 	Run: func(cobraCmd *cobra.Command, args []string) {
 		(&operations.ConsumerOperation{}).Consume(args[0], flags)
 	},
 }
 
 func init() {
-	CmdConsume.Flags().BoolVarP(&flags.PrintKeys, "print-keys", "k", false, "print message printKeys")
-	CmdConsume.Flags().BoolVarP(&flags.PrintTimestamps, "print-timestamps", "t", false, "print message printTimestamps")
-	CmdConsume.Flags().IntSliceP("partitions", "p", flags.Partitions, "partitions to consume")
+	CmdConsume.Flags().BoolVarP(&flags.PrintKeys, "print-keys", "k", false, "print message keys")
+	CmdConsume.Flags().BoolVarP(&flags.PrintTimestamps, "print-timestamps", "t", false, "print message timestamps")
+	CmdConsume.Flags().IntSliceP("partitions", "p", flags.Partitions, "partitions to consume. The default is to consume from all partitions.")
 	CmdConsume.Flags().BoolVarP(&flags.FromBeginning, "from-beginning", "b", false, "set offset for consumer to the oldest offset")
 	CmdConsume.Flags().StringVarP(&flags.OutputFormat, "output", "o", flags.OutputFormat, "Output format. One of: json|yaml")
 }
