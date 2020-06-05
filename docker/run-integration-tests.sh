39bcb38fc5562fb982759c9ee19ac3f5c3bb3035
Dirk Wilden <dirkwilden@gmail.com>
2020-06-05 22:09:03 +0200

    integration test different kafka versions

--- docker/run-integration-tests.sh
+++ docker/run-integration-tests.sh
@@ -6,9 +6,11 @@ set -o pipefail
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 ROOT_DIR="$(dirname "${SCRIPT_DIR}")"
 
+echo "using confluent platform version: ${CP_VERSION}"
+
 # docker compose up
 pushd ${ROOT_DIR}
-docker-compose -f ${SCRIPT_DIR}/docker-compose.yml up -d
+docker-compose -f ${SCRIPT_DIR}/docker-compose.yml --env-file=${SCRIPT_DIR}/.env up -d
 
 # docker compose down
 function tearDown {
