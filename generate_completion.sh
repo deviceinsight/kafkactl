4d36ee91ec738a36724b7d2ab164f53335d486c0
dwi-di <dirk.wilden@device-insight.com>
2020-08-03 06:57:49 +0000

    Deploying to gh-pages from  @ 0b154e3bc264efd59577cf7f56532015c0b39379 🚀

--- generate_completion.sh
+++ generate_completion.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 TARGET=$1
-PATH=$2
+BIN_PATH=$2
 
 if [ "linux_amd64" == "$TARGET" ]; then
-  echo "generating completions... ($PATH)"
+  PATH=`echo ${BIN_PATH} |xargs dirname`
+  echo "generating completions... $PATH"
+
+  pushd $PATH
   echo "" > /tmp/empty.yaml
-  $PATH completion bash > kafkactl-completion.bash --config-file=/tmp/empty.yaml
+  ./kafkactl completion bash > ../../kafkactl-completion.bash --config-file=/tmp/empty.yaml
+  popd
 fi
