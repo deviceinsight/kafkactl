1144c0947623cf8730e6328157f4bb25bfa757bf
Dirk Wilden <dirk.wilden@device-insight.com>
2020-08-11 09:36:14 +0200

    releases 1.11.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## 1.11.1 - 2020-08-11
+
 ## 1.11.0 - 2020-08-07
 
 ### Added
--- generate_completion.sh
+++ generate_completion.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+
 TARGET=$1
+BIN_PATH=$2
 
 if [ "linux_amd64" == "$TARGET" ]; then
-  echo "generating completions..."
+  echo "generating completions... ${BIN_PATH}"
 
   echo "" > /tmp/empty.yaml
-  ./kafkactl completion bash > dist/kafkactl-completion.bash --config-file=/tmp/empty.yaml
+  ${BIN_PATH} completion bash > ${SCRIPT_PATH}/kafkactl-completion.bash --config-file=/tmp/empty.yaml
 fi
