1ef0ecae702a0e32164fb92cc43e625163a76934
Dirk Wilden <dirk.wilden@device-insight.com>
2023-02-01 09:46:54 +0100

    replace deprecated function call

--- cmd/completion.go
+++ cmd/completion.go
@@ -40,7 +40,7 @@ $ kafkactl completion fish > ~/.config/fish/completions/kafkactl.fish
 `,
 		DisableFlagsInUseLine: true,
 		ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
-		Args:                  cobra.ExactValidArgs(1),
+		Args:                  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 		Run: func(cmd *cobra.Command, args []string) {
 
 			var err error
